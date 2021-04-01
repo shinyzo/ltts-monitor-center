cef0461d9acb94ed0bf1294dfcb389edc4e6b301
张良明 <zhangliangming@xjj.com>
2021-04-01 14:32:39 +0800

    1、监控中心加入sentinel

---
+++ ltts-sentinel-dashboard/bin/startup.sh
@@ -0,0 +1,4 @@
+java -jar ../lib/sentinel-dashboard-1.8.1.jar -Dserver.port=8718  -Dsentinel.dashboard.auth.username=sentinel -Dsentinel.dashboard.auth.password=123456 -Dserver.servlet.session.timeout=7200
+
+
+
